b76be7e725cafd2ff497298da411a9b5362fbf86
Xufang <xuzhaofangxzf@163.com>
2017-09-05 23:54:36 +0800

    第一次提交，并修改了ignore文件

---
+++ MyfirstPy.py
@@ -0,0 +1,13 @@
+#import antigravity
+import turtle
+turtle.forward(200)
+turtle.right(144)
+turtle.forward(200)
+turtle.right(144)
+turtle.forward(200)
+turtle.right(144)
+turtle.forward(200)
+turtle.right(144)
+turtle.forward(200)
+
+
